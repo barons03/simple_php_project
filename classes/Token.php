eac707e82533e23335e55d214372145545413863
barons03 <bkrisjanis@gmail.com>
2019-02-18 13:26:52 +0200

    Token generation

--- classes/Token.php
+++ classes/Token.php
@@ -11,6 +11,7 @@ class Token {
       Session::delete($tokenName);
       return true;
     }
+    
     return false;
   }
 }
--- core/init.php
+++ core/init.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+
 $GLOBALS['config'] = array(
   'mysql' => array(
     'host' => '127.0.0.1',
